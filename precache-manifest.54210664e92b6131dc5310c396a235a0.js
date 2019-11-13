105a0911af683f854aa92a495c8eeadae6468035
Douglas Ramaldes <douglasramaldes@gmail.com>
2019-11-13 18:21:01 -0300

    Updates

---
+++ precache-manifest.54210664e92b6131dc5310c396a235a0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dc5c8adbc003ef0ffe4d7681f35c76a",
+    "url": "/cep-maps/index.html"
+  },
+  {
+    "revision": "245e6220eba271a58dab",
+    "url": "/cep-maps/static/js/2.05245c9f.chunk.js"
+  },
+  {
+    "revision": "81a8d207ceaa8d5355e5",
+    "url": "/cep-maps/static/js/main.ea15e339.chunk.js"
+  },
+  {
+    "revision": "1223a6b1aed92c040f35",
+    "url": "/cep-maps/static/js/runtime-main.5c5e1b6b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5f0ec91fb55a1883c9d7051eccbe3cfa.js"
+  "/cep-maps/precache-manifest.54210664e92b6131dc5310c396a235a0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cep-maps/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
